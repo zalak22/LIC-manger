efe0514dd41cd485b23f4d8759c4664e5be6a52e
= <=>
2026-04-22 12:12:12 +0530

    add non-api user routes for account holders

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,13 +4,23 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 
+router.post("/users", authMiddleware, userController.createUser);
+
 router.post("/api/users", authMiddleware, userController.createUser);
+
+router.get("/users", authMiddleware, userController.getUsers);
 
 router.get("/api/users", authMiddleware, userController.getUsers);
 
+router.get("/users/:id", authMiddleware, userController.getUser);
+
 router.get("/api/users/:id", authMiddleware, userController.getUser);
 
+router.put("/users/:id", authMiddleware, userController.updateUser);
+
 router.put("/api/users/:id", authMiddleware, userController.updateUser);
+
+router.delete("/users/:id", authMiddleware, userController.deleteUser);
 
 router.delete("/api/users/:id", authMiddleware, userController.deleteUser);
 
